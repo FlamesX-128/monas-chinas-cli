ddedb9deb91643dfe4282b28ec90e611e5b92434
PanIntegralus <42538693+PanIntegralus@users.noreply.github.com>
2022-01-09 22:53:09 +0100

    Permitir cambiar el tamaño de la ventana
    Cambiar el valor "Resizable" en wails.CreateApp por true y borrar "MaxWidth" y "MaxHeight".

--- main.go
+++ main.go
@@ -36,11 +36,9 @@ func main() {
 		DisableInspector: true,
 		Height:           480,
 		HTML:             page,
-		Resizable:        false,
+		Resizable:        true,
 		MinWidth:         640,
 		MinHeight:        480,
-		MaxWidth:         640,
-		MaxHeight:        480,
 		Title:            "monas-chinas-cli",
 		Width:            640,
 	})
